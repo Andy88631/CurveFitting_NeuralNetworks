f921e27ff9ea64bd6028aab4c4e26d704f2e13f1
Ten-Ta Chiang <apple88631andy@gmail.com>
2018-09-23 10:56:30 +0800

    Add functions: Save all result to MAT file.

--- TrainModel_Dense.py
+++ TrainModel_Dense.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import scipy.io as sio
 from scipy.io import wavfile
 import batchup.data_source as data_source
 import os
-#from MFCC import mfcc_features
-#import TFrecord_Read
+
 
 """ Parameters """
 batch_size = 2
@@ -15,38 +15,46 @@ epoch      = 180
 learning_rate = 0.0001
 start_time = time.time()
 fonts = {'family' : 'Times New Roman'}
-modelSavePath = r"D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\Model_Dense\Model_Dense.ckpt"
-trainFilePath = r"D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\SweepSineData"
-testFilePath  = r"D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\1hrMusicData"
+musicType = 'Piano'
+
+modelSavePath = os.path.join(
+        r'D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\Model_Dense', 
+        musicType+'Data', 'Model_Dense_Funk.ckpt')
+
+#trainFilePath = r'D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\SweepSineData'
+trainFilePath = r'D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\1hrMusicData\ProcessedShortData'
+
+#testFilePath  = r'D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\1hrMusicData'
 
 
 """ Import data """
-trainFileList = os.listdir(trainFilePath)
 # Input training signal
-trainFlie = os.path.join(trainFilePath, trainFileList[1])
-print('Training file name of x: ', trainFileList[1])
+trainFileList = os.listdir(os.path.join(trainFilePath, 'input'))
+trainFlie = os.path.join(trainFilePath, 'input', trainFileList[0])
+print('Training file name of x: ', trainFileList[0])
 fs, x_train = wavfile.read(trainFlie)
 x_train = x_train/32768.0*10
 
 # Output training signal
-trainFlie = os.path.join(trainFilePath, trainFileList[0])
+trainFileList = os.listdir(os.path.join(trainFilePath, 'output'))
+trainFlie = os.path.join(trainFilePath, 'output', trainFileList[0])
 print('Training file name of y: ', trainFileList[0])
 fs, y_train = wavfile.read(trainFlie)
 y_train = y_train/32768.0*10
 
 # Input testing signal
-testFileList = os.listdir(os.path.join(testFilePath, 'input'))
-testFile = os.path.join(testFilePath, 'input', testFileList[0])
-print('Testing file name of x: ', testFileList[0])
-fs, x_test = wavfile.read(testFile)
-x_test = x_test/32768.0*10
+#testFileList = os.listdir(os.path.join(testFilePath, 'input'))
+#testFile = os.path.join(testFilePath, 'input', testFileList[0])
+#print('Testing file name of x: ', testFileList[0])
+#fs, x_test = wavfile.read(testFile)
+#x_test = x_test/32768.0*10
 
 # Output testing signal
-testFileList = os.listdir(os.path.join(testFilePath, 'output'))
-testFile = os.path.join(testFilePath, 'output', testFileList[0])
-print('Testing file name of y: ', testFileList[0])
-fs, y_test = wavfile.read(testFile)
-y_test = y_test/32768.0*10
+#testFileList = os.listdir(os.path.join(testFilePath, 'output'))
+#testFile = os.path.join(testFilePath, 'output', testFileList[0])
+#print('Testing file name of y: ', testFileList[0])
+#fs, y_test = wavfile.read(testFile)
+#y_test = y_test/32768.0*10
 
 
 """ Data preprocessing """
@@ -102,23 +110,34 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True),
 
     sess.run(tf.global_variables_initializer())
           
-    LVObj      = []
-    meanLV     = []
+    trainLVObj = []
+    meanTrainLV= []
+    validLVObj = []
+    meanValidLV= []
     meanRS     = []
     validRSObj = []
     trainRSObj = []
+    TrainPredictData = []
+    TrainTrueData    = []
+    ValidPredictData = []
+    ValidTrueData    = []
     trainDataSets = data_source.ArrayDataSource([x_train_data, y_train_data], repeats=1)
+    i = 0   # Training iteration
+    vi= 0   # Validation iteration
     for e in range(epoch):
-        """ Training loss """
-        i = 0
+        """ Training loss """   
         for (x_input, y_input) in trainDataSets.batch_iterator(batch_size=batch_size):
             feed = {x:x_input, y:y_input, learning_rate_:learning_rate}
             trainLV, _, Predict_trainValue, True_trainOutput = sess.run([cost,optimizer,yOut,y2],
                                                                         feed_dict=feed)  
-            LVObj.append(trainLV)
+            trainLVObj.append(trainLV)   
             i += 1
-            if i % 10 == 0:
-                meanLV.append(np.mean(LVObj))  # Compute the average loss value of 10 batchs
+            if i % 10 == 0:     # Compute the average loss value of 10 batchs
+                meanTrainLV.append(np.mean(trainLVObj))  
+                
+            if e == epoch-1:
+                TrainPredictData = np.append(TrainPredictData, Predict_trainValue)
+                TrainTrueData    = np.append(TrainTrueData, True_trainOutput)
                 
             """ Plot """
             print(trainLV)
@@ -130,8 +149,8 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True),
             plt.rc('font', **fonts)
             plt.xlabel('Iteration')
             plt.ylabel('Loss value')
-            plt.title('Loss value: %s' %trainLV + ' , Epoch: %s' %e)
-            plt.plot(meanLV)
+            plt.title('Training loss, Loss value: %s' %trainLV + ', Iteration: %i' %i)
+            plt.plot(meanTrainLV)
             plt.draw()
             plt.pause(1e-17)
             plt.grid(True)
@@ -151,22 +170,29 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True),
             plt.show()
     
         """ Save model """
-        if e % 20 == 0:
+        if e % 60 == 0:
             tf.train.Saver().save(sess, modelSavePath, global_step=e)
             print('Model of No. %f epoch saved!' %e)
         
-        """ Compute Training & Validation R-Square """
+        """ Validation  """
         validationDataSets = data_source.ArrayDataSource([x_validation_data, 
                                                           y_validation_data], repeats=1)
         for (x_input, y_input) in validationDataSets.batch_iterator(batch_size=batch_size):
             feed = {x:x_input, y:y_input, learning_rate_:learning_rate}
             validLV, _, Predict_validValue, True_validOutput = sess.run([cost,optimizer,yOut,y2],
-                                                                             feed_dict=feed)  
+                                                                             feed_dict=feed)
+            validLVObj.append(validLV)   
+            vi += 1
+            if vi % 10 == 0:     # Compute the average loss value of 10 batchs
+                meanValidLV.append(np.mean(validLVObj)) 
+                
+            if e == epoch-1:
+                ValidPredictData = np.append(ValidPredictData, Predict_validValue)
+                ValidTrueData    = np.append(ValidTrueData, True_validOutput)
+            
             # R-Square (coefficient of determination)
             validRS = (np.corrcoef(Predict_validValue, True_validOutput))**2
             validRSObj.append(validRS[0,1])
-#                trainRS = (np.corrcoef(True_trainOutput,Predict_validValue))**2
-#                trainRSObj.append(trainRS[0,1])
         meanRS.append(np.mean(validRSObj))
         
         """ Plot """
@@ -178,9 +204,8 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True),
         plt.rc('font', **fonts)
         plt.xlabel('Epoch')
         plt.ylabel('R-Square value')
-        plt.title('Coefficient of determination')
+        plt.title('Coefficient of determination' + ', Epoch: %s' %e)
         plt.plot(meanRS, label='Validation')
-#            plt.plot(trainRSObj, label='Train')
         plt.legend(loc='upper left')
         plt.draw()
         plt.pause(1e-17)
@@ -190,10 +215,19 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True),
     # Save the final model
     tf.train.Saver().save(sess, modelSavePath, global_step=e)
     
+    # Write to MAT file
+    SavePath = r'D:\Dropbox\MachineLearning\CurveFitting_SystemIdentification\Result\TrainingOutput'
+    dict_data= {'Train_Predict': TrainPredictData, 'Train_True': TrainTrueData,
+                'Valid_Predict': ValidPredictData, 'Valid_True': ValidTrueData,
+                'Train_Loss': meanTrainLV, 'Valid_Loss': meanValidLV, 'R_Square': meanRS[e]}
+    sio.savemat(os.path.join(SavePath, 'DenseResult_'+musicType+'.mat'), dict_data)
+    
     # Count time
     training_time = time.time() - start_time
     training_time = time.strftime("%H:%M:%S", time.gmtime(training_time))
     print('Training time: ' + str(training_time))
+    
+    print('Final R-square value: %s' %meanRS[e])
  
     # Memory recycle
     sess.close() 
